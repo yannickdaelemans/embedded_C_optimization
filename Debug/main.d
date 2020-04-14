529831185b84759855d74720f78a4a95ccbdd83f
Yannick.Daelemans <yannick.daelemans@student.kuleuven.be>
2020-04-14 11:49:31 +0200

    some clean up and adjusting the main

--- Debug/main.d
+++ Debug/main.d
@@ -1,7 +1,8 @@
 main.o: ../main.c C:/ti/ccs1000/ccs/ccs_base/msp430/include_gcc/msp430.h \
  C:/ti/ccs1000/ccs/ccs_base/msp430/include_gcc/msp430fr6989.h \
  C:/ti/ccs1000/ccs/ccs_base/msp430/include_gcc/iomacros.h \
- C:/ti/ccs1000/ccs/ccs_base/msp430/include_gcc/in430.h
+ C:/ti/ccs1000/ccs/ccs_base/msp430/include_gcc/in430.h \
+ ../UART_communication/UART_communication.h
 
 C:/ti/ccs1000/ccs/ccs_base/msp430/include_gcc/msp430.h:
 
@@ -10,3 +11,5 @@ C:/ti/ccs1000/ccs/ccs_base/msp430/include_gcc/msp430fr6989.h:
 C:/ti/ccs1000/ccs/ccs_base/msp430/include_gcc/iomacros.h:
 
 C:/ti/ccs1000/ccs/ccs_base/msp430/include_gcc/in430.h:
+
+../UART_communication/UART_communication.h:
--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <msp430.h> 
+#include "UART_communication/UART_communication.h"
 
 
 /**
@@ -8,12 +9,14 @@ int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 	
-	// Startup clock system with max DCO setting ~8MHz
-	  CSCTL0_H = CSKEY >> 8;                    // Unlock clock registers
-	  CSCTL1 = DCOFSEL_3 | DCORSEL;             // Set DCO to 8MHz
-	  CSCTL2 = SELA__VLOCLK | SELS__DCOCLK | SELM__DCOCLK;
-	  CSCTL3 = DIVA__1 | DIVS__1 | DIVM__1;     // Set all dividers
-	  CSCTL0_H = 0;                             // Lock CS registers
+	init_GPIO();
+	init_clocks();
+	init_communication();
+
+	// For debugging purposes
+	while(1){
+	    send_data(0x00FF);
+	}
 
 	return 0;
 }
