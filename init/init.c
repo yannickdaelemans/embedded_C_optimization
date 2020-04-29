6fff70c732047ed1bf58397addbc84f13ea96402
Yannick.Daelemans <yannick.daelemans@student.kuleuven.be>
2020-04-29 14:43:02 +0200

    Tested clocks, must work with 8MHz clock to get the best result

--- init/init.c
+++ init/init.c
@@ -24,20 +24,14 @@ void init_clocks (void){
 
     // Configure one FRAM waitstate as required by the device datasheet for MCLK
     // operation beyond 8MHz _before_ configuring the clock system.
-    FRCTL0 = FRCTLPW | NWAITS_1;
+    //FRCTL0 = FRCTLPW | NWAITS_1;
 
     // Clock System Setup
-    CSCTL0_H = CSKEY_H;                     // Unlock CS registers
-    CSCTL1 = DCOFSEL_0;                     // Set DCO to 1MHz
-    // Set SMCLK = MCLK = DCO, ACLK = LFXTCLK (VLOCLK if unavailable)
-    CSCTL2 = SELA__LFXTCLK | SELS__DCOCLK | SELM__DCOCLK;
-    // Per Device Errata set divider to 4 before changing frequency to
-    // prevent out of spec operation from overshoot transient
-    CSCTL3 = DIVA__4 | DIVS__4 | DIVM__4;   // Set all corresponding clk sources to divide by 4 for errata
-    CSCTL1 = DCOFSEL_4 | DCORSEL;           // Set DCO to 16MHz
-    // Delay by ~10us to let DCO settle. 60 cycles = 20 cycles buffer + (10us / (1/4MHz))
-    __delay_cycles(60);
-    CSCTL3 = DIVA__1 | DIVS__1 | DIVM__1;   // Set all dividers to 1 for 16MHz operation
+    CSCTL0_H = CSKEY >> 8;                    // Unlock CS registers
+    CSCTL1 = DCOFSEL_6;                       // Set DCO to 8MHz
+    CSCTL2 = SELA__LFXTCLK | SELS__DCOCLK | SELM__DCOCLK;  // Set SMCLK = MCLK = DCO
+                                                // ACLK = VLOCLK
+    CSCTL3 = DIVA__1 | DIVS__1 | DIVM__1;     // Set all dividers to 1
 
     CSCTL4 &= ~LFXTOFF;
     do
--- main.c
+++ main.c
@@ -6,6 +6,9 @@
 /**
  * main.c
  */
+
+char global_variable = 0b01010101;
+
 int main(void)
 {
 
@@ -20,25 +23,88 @@ int main(void)
 	return 0;
 }
 
+void sending(unsigned int ID, unsigned int timer_begin, unsigned int timer_end){
+    unsigned int time;
+    time = timer_end - timer_begin;
+    send_data(ID);
+    send_data(timer_begin);
+    send_data(timer_end);
+    send_data(time);
+    send_data(0x0000);
+}
+
+int testing_clock_call_function(int* pi, int* pi2){
+    char bits = 0b11110000;
+    bits ^= bits >> 3;
+    bits = bits ^ global_variable;
+
+    int var = *pi + *pi2;
+    return var+bits;
+}
+
+void testing_clock(){
+    unsigned int ID = 0x0000;
+    unsigned int timer_begin = TA0R;;
+    unsigned int timer_end;
+    // start with addressing some pins
+    //declare all the pins
+    //Control Ports
+    P3DIR |= 0b00000000; // make port read only
+
+    // make some pins write
+    P1DIR |= 0b11111000;
+    P8DIR |= 0b11110000;
+    P9DIR |= 0b01111111;
+    P2DIR |= 0xFF;
+
+    char num = 0x80;
+    P2OUT |= num;
+
+    int number = 10;
+    int* pi = &number;
+    int number2 = 20;
+    int* pi2 = &number2;
+
+    int result = testing_clock_call_function(pi, pi2);
+    for( int i = 0; i < 3; i++){
+        if (result > 100){
+            do{
+                result = result - 1;
+            } while(result > 100);
+        }else{
+            for(int i = 0; i < 5; ++i){
+                result = result + i;
+            }
+        }
+    }
+    timer_end = TA0R;
+    sending(ID, timer_begin, timer_end);
+}
+
+
 void testing(){
+    unsigned int ID = 0x0000;
     unsigned int timer_begin;
     unsigned int timer_end;
     unsigned int time;
     int amount = 0;
     // For debugging purposes
     timer_begin = TA0R;
-    for(int i = 0; i < 20000; i ++){
-        __no_operation();
-     }
+    for(int i = 0; i <10; i++){
+        for(int i = 0; i < 20000; i ++){
+            __no_operation();
+        }
+    }
+
     timer_end = TA0R;
     time = timer_end - timer_begin;
+    send_data(ID);
     send_data(timer_begin);
     send_data(timer_end);
     send_data(time);
     send_data(0x0000);
     amount++;
 }
-
 
 #if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
 #pragma vector=USCI_A1_VECTOR
@@ -57,7 +123,7 @@ void __attribute__ ((interrupt(USCI_A1_VECTOR))) USCI_A1_ISR (void)
       UCA1IFG &= ~UCRXIFG;
       char read_buf = UCA1RXBUF;
       if(read_buf == '1'){
-          testing();
+          testing_clock();;
       }
       __no_operation();
       break;
