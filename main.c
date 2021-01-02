e2b46a2dcd36cb54697dfcde7011ccbd0bd23c0d
yannick.daelemans <yannick.daelemans@student.kuleuven.be>
2021-01-02 19:23:58 +0100

    cleaning

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
  * Created by:  Yannick Daelemans
  *
  * This will test all the methods as discussed in the thesis.
- * It will time all the methods and will send this through to a PC.
+ * It will time all the methods and will send this through to a PC over UART.
  */
 
 /*
@@ -44,79 +44,7 @@ void __attribute__ ((noinline)) sending(unsigned int ID, unsigned int timer_begi
     send_data(0xFFFF);
 }
 
-/*
-int testing_clock_call_function(int* pi, int* pi2){
-    char bits = 0b11110000;
-    bits ^= bits >> 3;
-    bits = bits ^ global_variable;
 
-    int var = *pi + *pi2;
-    return var+bits;
-}
-
-void testing_clock(){
-    unsigned int ID = 0x0000;
-    unsigned int timer_begin = TA0R;
-    unsigned int timer_end;
-    // start with addressing some pins
-    //declare all the pins
-    //Control Ports
-    P3DIR |= 0b00000000; // make port read only
-
-    // make some pins write
-    P1DIR |= 0b11111000;
-    P8DIR |= 0b11110000;
-    P9DIR |= 0b01111111;
-    P2DIR |= 0xFF;
-
-    char num = 0x80;
-    P2OUT |= num;
-
-    int number = 10;
-    int* pi = &number;
-    int number2 = 20;
-    int* pi2 = &number2;
-
-    int result = testing_clock_call_function(pi, pi2);
-    for( int i = 0; i < 3; i++){
-        if (result > 100){
-            do{
-                result = result - 1;
-            } while(result > 100);
-        }else{
-            for(int i = 0; i < 5; ++i){
-                result = result + i;
-            }
-        }
-    }
-    timer_end = TA0R;
-    sending(ID, timer_begin, timer_end);
-}
-
-
-void testing(){
-    unsigned int ID = 0x0000;
-    unsigned int timer_begin;
-    unsigned int timer_end;
-    unsigned int time;
-    int amount = 0;
-    // For debugging purposes
-    timer_begin = TA0R;
-    for(int i = 0; i <10; i++){
-        for(int i = 0; i < 20000; i ++){
-            __no_operation();
-        }
-    }
-
-    timer_end = TA0R;
-    time = timer_end - timer_begin;
-    send_data(ID);
-    send_data(timer_begin);
-    send_data(timer_end);
-    send_data(time);
-    send_data(0x0000);
-    amount++;
-}
 
 /*
  * Tester of all timings that need to be timed
